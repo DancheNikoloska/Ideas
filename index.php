cce9f9dfb6659daa5200d876ba3276d2283957bc
Goran <gg.goran93@gmail.com>
2015-07-14 16:23:29 +0200

    hk

--- index.php
+++ index.php
@@ -9,21 +9,27 @@ include 'database.php';
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Имам идеја</title>
+     <script src="js/jquery.js"></script>
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/prettyPhoto.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
+     <link href="css/test.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/test.css" rel="stylesheet">
+   
     <link rel="shortcut icon" href="images/ico/favicon.ico">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
     
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head><!--/head-->
 
 <body data-spy="scroll" data-target="#navbar" data-offset="0">
@@ -42,22 +48,55 @@ include 'database.php';
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="#main-slider"><i class="icon-home"></i></a></li>
-                        <li><a href="#addIdea" data-toggle="modal" data-target=".bs-modal-sm">Додај идеја</a></li>
+                        <li><a href="#addIdea" data-toggle="modal" data-target=".bs-modal-sm">Додај идеја</a></li> 
                         <li><a href="#portfolio">Portfolio</a></li>
                         <li><a href="#pricing">Pricing</a></li>
                         <li><a href="#about-us">About Us</a></li>
                         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-			<ul id="login-dp" class="dropdown-menu">
-			
-			
-                    </ul>
+		                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" ><b>Login</b> <span class="caret"></span></a>
+							<ul id="login-dp" class="dropdown-menu">
+								<li>
+									 <div class="row">
+											<div class="col-md-12">
+												Login via
+												<div class="social-buttons">
+													<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+													<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+												</div>
+				                                or
+												 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+														<div class="form-group">
+															 <label class="sr-only" for="exampleInputEmail2">Email address</label>
+															 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+														</div>
+														<div class="form-group">
+															 <label class="sr-only" for="exampleInputPassword2">Password</label>
+															 <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+				                                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
+														</div>
+														<div class="form-group">
+															 <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+														</div>
+														<div class="checkbox">
+															 <label>
+															 <input type="checkbox"> keep me logged-in
+															 </label>
+														</div>
+												 </form>
+											</div>
+											<div class="bottom text-center">
+												New here ? <a href="#"><b>Join Us</b></a>
+											</div>
+									 </div>
+								</li>
+							</ul>
+			            </li>
                 </div>
             </div>
         </div>
     </header><!--/#header-->
     
-    <!-- Modal -->
+  	  <!-- Modal -->
 <div class="modal fade bs-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-sm modal-margin">
     <div class="modal-content">
@@ -456,10 +495,10 @@ include 'database.php';
         </div>
     </footer><!--/#footer-->
 
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+   
+    
     <script src="js/jquery.isotope.min.js"></script>
     <script src="js/jquery.prettyPhoto.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/main.js"></script> 
 </body>
 </html>
