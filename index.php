c207351ad172680c608b5180d940ae09b46b5f6d
Goran <gg.goran93@gmail.com>
2015-07-14 16:48:32 +0200

    Tabs registration

--- index.php
+++ index.php
@@ -52,42 +52,31 @@ include 'database.php';
                         <li><a href="#portfolio">Portfolio</a></li>
                         <li><a href="#pricing">Pricing</a></li>
                         <li><a href="#about-us">About Us</a></li>
-                        <li class="dropdown">
-		                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" ><b>Login</b> <span class="caret"></span></a>
+                        <li class="dropdown mega-dropdown">
+		                   <a href="#" class="dropdown-toggle"  ><b>Login</b> <span class="caret"></span></a>
 							<ul id="login-dp" class="dropdown-menu">
 								<li>
-									 <div class="row">
-											<div class="col-md-12">
-												Login via
-												<div class="social-buttons">
-													<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-													<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-												</div>
-				                                or
-												 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-														<div class="form-group">
-															 <label class="sr-only" for="exampleInputEmail2">Email address</label>
-															 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-														</div>
-														<div class="form-group">
-															 <label class="sr-only" for="exampleInputPassword2">Password</label>
-															 <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-				                                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
-														</div>
-														<div class="form-group">
-															 <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-														</div>
-														<div class="checkbox">
-															 <label>
-															 <input type="checkbox"> keep me logged-in
-															 </label>
-														</div>
-												 </form>
-											</div>
-											<div class="bottom text-center">
-												New here ? <a href="#"><b>Join Us</b></a>
-											</div>
+								   <div class="row">
+									<div class="col-md-12">
+									 <!-- Nav tabs -->
+									  <ul class="nav nav-tabs" role="tablist">
+									    <li role="presentation" class="active"><a href="#Login" aria-controls="home" role="tab" data-toggle="tab">Најава</a></li>
+									    <li role="presentation"><a href="#Register" aria-controls="profile" role="tab" data-toggle="tab">Регистрација</a></li>
+									    
+									  </ul>
+									
+									  <!-- Tab panes -->
+									  <div class="tab-content">
+									    <div role="tabpanel" class="tab-pane active" id="Login">
+                                             Ime<input class="form-control" type="text">
+                                             Prezime<input class="form-control" type="text">
+                                        </div>
+									    <div role="tabpanel" class="tab-pane" id="Register">
+									    	NASNDNSDASD
+									    </div>
+									  </div>
 									 </div>
+									</div>
 								</li>
 							</ul>
 			            </li>
@@ -500,5 +489,18 @@ include 'database.php';
     <script src="js/jquery.isotope.min.js"></script>
     <script src="js/jquery.prettyPhoto.js"></script>
     <script src="js/main.js"></script> 
+    <script>
+	  $(function() {
+	    $( "#tabs" ).tabs();
+	  });
+	  $('li.dropdown.mega-dropdown a').on('click', function (event) {
+    	$(this).parent().toggleClass('open');
+	  });
+	  $('body').on('click', function (e) {
+    	if (!$('li.dropdown.mega-dropdown').is(e.target) && $('li.dropdown.mega-dropdown').has(e.target).length === 0 && $('.open').has(e.target).length === 0) {
+        	$('li.dropdown.mega-dropdown').removeClass('open');
+    	}
+	  });
+    </script>
 </body>
 </html>
